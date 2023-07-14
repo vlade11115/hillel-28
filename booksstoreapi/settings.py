230b482233685ae9b7ac7444d207bcba5f85dd2c
Vladyslav Kartavets <vladyslav.kartavets@staircase.co>
2023-07-14 20:48:53 +0300

    Debug

--- booksstoreapi/settings.py
+++ booksstoreapi/settings.py
@@ -45,6 +45,7 @@ IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 # SECURITY WARNING: don't run with debug turned on in production!
 if not IS_HEROKU_APP:
     DEBUG = True
+DEBUG = True
 
 # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
 # validation of the Host header in the incoming HTTP request. On other platforms you may need
